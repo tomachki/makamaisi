d917e09ccbb8670c74fb3ac72c12dfb18472782c
tomachki <check.everything1@gmail.com>
2018-07-07 23:22:34 +0400

    Added drop-down list(No arrow icon next to dropdown)

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// 
+//
+// document.getElementById('dropBtn').addEventListener('onmouseover', dropMenu());
+//  document.getElementById('dropBtn').addEventListener('onmouseout', shrinkMenu());
+//
+// function dropMenu() {
+//   document.querySelector('.drop-items').style.display = 'block';
+//   document.querySelector('.dropdown').style.backgroundColor = '#f4f4f4';
+// }
+//
+// function shrinkMenu() {
+//   document.querySelector('.drop-items').style.display = 'none';
+//   document.querySelector('.dropdown').style.backgroundColor = 'white';
+// }
